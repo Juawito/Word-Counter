6da5ea3afaaa1e9dbb13a478f84dac6802868cc1
Juawito <santiagojuan311@gmail.com>
2022-04-04 22:24:17 -0400

    creating worker object in blob format to read function from worker.js

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,25 @@
-const h1 = document.getElementById('counter');
+const h1 = document.getElementById('wordCount');
 const body = document.getElementsByTagName('body');
+const button = document.querySelector('.button');
+let counterWorker = new Worker(URL.createObjectURL(new Blob(["("+workerFunction.toString()+")()"], {type: 'text/javascript'})));
+
 
 const testJs = () => {
-    h1.classList.add('text-red-700');
-    const h2 = document.createElement('h2');
-    h2.innerHTML = 'Hello World'
-    body.appendChild(h2)
+    h1.setAttribute('class','text-red-700');
+    // const h2 = document.createElement('h2');
+    // h2.innerHTML = 'Hello World'
+    // body.appendChild(h2)
 
+
+}
+let num = 10
+button.addEventListener('click', () => {
+    counterWorker.postMessage(num);
+    console.log('clicked')
+})
+counterWorker.onmessage = ({data}) => {
+    const response = data;
+    console.log('received data')
 }
 
 testJs()
